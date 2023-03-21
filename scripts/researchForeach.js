a1b14a7e1130c0ae85ffc911cac9ae1d304e9e0b
k2rDev75 <k2rdev@gmail.com>
2023-03-21 16:57:03 +0100

    Ajout des commentaires dans le code algo 2

--- scripts/researchForeach.js
+++ scripts/researchForeach.js
@@ -64,60 +64,81 @@
 //     document.querySelector(".error_research").style.display = "none";
 //   }
 // });
+// Fonction pour effectuer une recherche globale dans les recettes
 function globalSearch() {
+  // Récupère la valeur du champ de recherche, la convertit en minuscules et supprime les espaces au début et à la fin
   const searchField = document
     .getElementById("search")
     .value.toLowerCase()
     .trim();
 
+  // Initialise un tableau vide pour stocker les recettes correspondantes
   const inputArray = [];
 
+  // Parcourt toutes les recettes
   recipes.forEach((recipe) => {
+    // Vérifie si le nom de la recette contient le texte recherché
     if (recipe.name.toLowerCase().includes(searchField)) {
+      // Ajoute la recette au tableau inputArray
       inputArray.push(recipe);
     } else {
+      // Parcourt tous les ingrédients de la recette
       recipe.ingredients.forEach((ingredient) => {
+        // Vérifie si un ingrédient contient le texte recherché
         if (ingredient.ingredient.toLowerCase().includes(searchField)) {
+          // Ajoute la recette au tableau inputArray
           inputArray.push(recipe);
         }
       });
+      // Vérifie si la description de la recette contient le texte recherché
       if (recipe.description.toLowerCase().includes(searchField)) {
+        // Ajoute la recette au tableau inputArray
         inputArray.push(recipe);
       }
     }
   });
 
+  // Affiche le tableau inputArray dans la console
   console.log(
     "🚀 ~ file: research.js:28 ~ inputArray ~ inputArray:",
     inputArray
   );
-
+  // Affiche ou masque le message d'erreur en fonction du nombre de résultats de recherche
   if (inputArray.length === 0) {
     document.querySelector(".error_research").style.display = "block";
   } else {
     document.querySelector(".error_research").style.display = "none";
   }
 
+  // Affiche les recettes correspondantes en supprimant les doublons à l'aide de l'objet Set
   displayRecipe([...new Set(inputArray)]);
 }
 
+// Sélectionne le bouton de recherche
 const buttonSearch = document.querySelector("button");
 
+// Ajoute un écouteur d'événement click au bouton de recherche pour appeler la fonction globalSearch
 buttonSearch.addEventListener("click", () => {
   globalSearch();
 });
 
+// Sélectionne l'élément d'entrée de recherche
 const searchInput = document.querySelector("#search");
 
+// Ajoute un écouteur d'événement input à l'élément d'entrée de recherche
 searchInput.addEventListener("input", () => {
+  // Si la longueur du texte est supérieure ou égale à 3 caractères, appelle la fonction globalSearch et masque le message d'erreur "search_error"
   if (searchInput.value.length >= 3) {
     globalSearch();
     document.querySelector(".search_error").style.display = "none";
   }
+  // Si la longueur du texte est supérieure à 0 et inférieure à 3 caractères, affiche les recettes d'origine et affiche le message d'erreur "search_error"
   if (searchInput.value.length > 0 && searchInput.value.length < 3) {
     displayRecipe(recipes);
     document.querySelector(".search_error").style.display = "block";
   }
+  // Si la longueur du texte est égale à 0, affiche les recettes d'origine et masque les messages d'erreur "search_error" et "error_research"
+  // Si la longueur du texte est égale à 0, affiche les recettes d'origine et masque les messages d'erreur "search_error" et "error_research"
   if (searchInput.value.length === 0) {
     displayRecipe(recipes);
     document.querySelector(".search_error").style.display = "none";
